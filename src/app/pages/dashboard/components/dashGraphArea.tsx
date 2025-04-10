bcd844ed0d8b027c49cb7d8bbc2ce586e5f4e538
pedro <pedrocp010102@gmail.com>
2025-04-10 16:08:51 -0300

    finish dash

--- src/app/pages/dashboard/components/dashGraphArea.tsx
+++ src/app/pages/dashboard/components/dashGraphArea.tsx
@@ -1,5 +1,5 @@
-import { TrendingUp } from "lucide-react"
-import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from "recharts"
+import { TrendingUp } from "lucide-react";
+import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from "recharts";
 
 import {
   Card,
@@ -8,41 +8,62 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
-} from "@/components/ui/chart"
-const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
-]
+} from "@/components/ui/chart";
+import {
+  GroupedChartData,
+  groupTransactionsByMonth,
+} from "@/utils/groupTransactionsByMonth";
+import { useEffect, useState } from "react";
+import { useByUserTransactionsData } from "@/hooks/transactions/useByUserTransactionsData";
+import { useAuth } from "@/store/auth";
 
 const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "hsl(var(--chart-1))",
+  income: {
+    label: "Receitas",
+    color: "#05df72",
   },
-  mobile: {
-    label: "Mobile",
-    color: "hsl(var(--chart-2))",
+  expense: {
+    label: "Despesas",
+    color: "#fb2c36",
   },
-} satisfies ChartConfig
+} satisfies ChartConfig;
 
 export function DashGraphArea() {
+  const { user } = useAuth();
+  const [chartData, setChartData] = useState<GroupedChartData[]>([]);
+
+  useEffect(() => {
+    async function fetchTransactions() {
+      if (!user) {
+        throw new Error("Usuário não autenticado.");
+      }
+
+      try {
+        const transactionsData = await useByUserTransactionsData(user.id);
+
+        const groupedData: GroupedChartData[] =
+          groupTransactionsByMonth(transactionsData);
+
+        setChartData(groupedData);
+      } catch (error) {
+        console.error("Erro ao buscar transações", error);
+      }
+    }
+
+    fetchTransactions();
+  }, [user]);
+
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Area Chart - Axes</CardTitle>
-        <CardDescription>
-          Showing total visitors for the last 6 months
-        </CardDescription>
+        <CardTitle>Área de Receita vs Despesa</CardTitle>
+        <CardDescription>Visualização das transações</CardDescription>
       </CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig}>
@@ -66,23 +87,25 @@ export function DashGraphArea() {
               tickLine={false}
               axisLine={false}
               tickMargin={8}
-              tickCount={3}
+              tickCount={11}
+              domain={[0, 10000]}
+              ticks={Array.from({ length: 11 }, (_, i) => i * 1000)} 
             />
             <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
             <Area
-              dataKey="mobile"
+              dataKey="income"
               type="natural"
-              fill="var(--color-mobile)"
+              fill={chartConfig.income.color}
               fillOpacity={0.4}
-              stroke="var(--color-mobile)"
+              stroke={chartConfig.income.color}
               stackId="a"
             />
             <Area
-              dataKey="desktop"
+              dataKey="expense"
               type="natural"
-              fill="var(--color-desktop)"
+              fill={chartConfig.expense.color}
               fillOpacity={0.4}
-              stroke="var(--color-desktop)"
+              stroke={chartConfig.expense.color}
               stackId="a"
             />
           </AreaChart>
@@ -92,14 +115,15 @@ export function DashGraphArea() {
         <div className="flex w-full items-start gap-2 text-sm">
           <div className="grid gap-2">
             <div className="flex items-center gap-2 font-medium leading-none">
-              Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+              Tendência de alta de 5,2% neste mês{" "}
+              <TrendingUp className="h-4 w-4" />
             </div>
             <div className="flex items-center gap-2 leading-none text-muted-foreground">
-              January - June 2024
+              Meses de 2025 até agora
             </div>
           </div>
         </div>
       </CardFooter>
     </Card>
-  )
+  );
 }
